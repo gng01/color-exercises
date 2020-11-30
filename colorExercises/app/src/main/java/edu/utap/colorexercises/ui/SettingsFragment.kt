e95454e553cd64eed1215b8a3fcbf3bb62ffa384
arky <qyangaa@gmail.com>
2020-11-30 15:45:05 -0800

    Fixed action_settings reference issue

--- colorExercises/app/src/main/java/edu/utap/colorexercises/ui/SettingsFragment.kt
+++ colorExercises/app/src/main/java/edu/utap/colorexercises/ui/SettingsFragment.kt
@@ -1,24 +1,16 @@
 package edu.utap.colorexercises.ui
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.UserProfileChangeRequest
-import edu.utap.colorexercises.AuthInitActivity
 import edu.utap.colorexercises.MainActivity
 import edu.utap.colorexercises.R
-import edu.utap.colorexercises.model.MainViewModel
-import kotlinx.android.synthetic.main.fragment_home.*
 
 class SettingsFragment :
     Fragment(R.layout.fragment_settings) {
@@ -30,13 +22,9 @@ class SettingsFragment :
         }
     }
 
-    private fun initButtons(root: View) {
-        val cancelNameButton = root.findViewById<Button>(R.id.btn_cancelName)
+    private fun initSubmitButton(root: View) {
         val submitNameButton = root.findViewById<Button>(R.id.btn_submitName)
         val enterNameET = root.findViewById<EditText>(R.id.ed_changeName)
-        cancelNameButton.setOnClickListener {
-            parentFragmentManager.popBackStack()
-        }
         submitNameButton.setOnClickListener {
             (activity as MainActivity).hideKeyboard()
 
@@ -51,7 +39,6 @@ class SettingsFragment :
                     if (it.isSuccessful) {
                         Log.d(TAG, "Submission successful");
                         parentFragmentManager.popBackStack()
-
                     }else{
                         Toast.makeText(context, "Submission Failed, please try again", Toast.LENGTH_LONG)
                     }
@@ -62,9 +49,18 @@ class SettingsFragment :
         }
     }
 
+    private fun initCancelButton(root: View) {
+        val cancelNameButton = root.findViewById<Button>(R.id.btn_cancelName)
+        cancelNameButton.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initButtons(view)
+        initSubmitButton(view)
+        initCancelButton(view)
     }
 
 }
